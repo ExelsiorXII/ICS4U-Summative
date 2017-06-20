28a29a4a9901bb8f9783bc70ad2ed50fe9c20970
ExelsiorXII <david.zhu.h@gmail.com>
2017-06-20 12:23:06 -0400

    Update song1.ino

--- song1.ino
+++ song1.ino
@@ -71,15 +71,11 @@ int noteDurations[] = {
     int size = sizeof(melody) / sizeof(int);
     for (int thisNote = 0; thisNote < size; thisNote++) {
  
-      // to calculate the note duration, take one second
-      // divided by the note type.
-      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      // quarter note = 1000 / 4, eighth note = 1000/8, etc.
       int noteDuration = 1000 / noteDurations[thisNote];
  
       tone(8, melody[thisNote], noteDuration);
  
-      // to distinguish the notes, set a minimum time between them.
-      // the note's duration + 30% seems to work well:
       int pauseBetweenNotes = noteDuration * 1.30;
       delay(pauseBetweenNotes);
  
